88d064ba3fcc3de258bdeafd5db9b4cff5bdebaa
akshay <akshaymishra5395@gmail.com>
2023-08-24 02:46:04 +0530

    changes

--- 1.4 Stack using two Queue.py
+++ 1.4 Stack using two Queue.py
@@ -6,7 +6,20 @@ class Stack:
         self.q2=Queue()
 
     def push(self,x):
-        
+        pass
 
     def pop(self):
+        pass
         
+if __name__ == '__main__':
+    sq = Stack()
+    sq.push(1)
+    sq.push(2)
+    sq.push(3)
+    print(sq.pop())
+    sq.push(4)
+    print(sq.pop())
+    print(sq.pop())
+    print(sq.pop())
+    print(sq.pop())
+    print()
