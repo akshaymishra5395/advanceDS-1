7415c7203dc813323a8ecc2bf89e55eb72edf636
akshay <akshaymishra5395@gmail.com>
2023-08-24 00:19:29 +0530

    anagram final

--- 4.Anagram_Problem.py
+++ 4.Anagram_Problem.py
@@ -2,9 +2,9 @@ class GFG:
     def __init__(self):
         self.R = None
         self.C = None
-        self.dir = [[-1, -1], [-1, 0], [-1, 1],
-                    [0, -1],[0,0], [0, 1], [-1, 1],
-                    [0, 1], [1 , 1]]
+        self.dir = [[-1, 1], [0, 1], [1, 1],
+                    [1, 0],[1,-1],[0,-1 ], [-1, -1],
+                    [-1, 0]]
     
     def search(self,matrix,row,col,word):
         if matrix[row][col] != word[0]:
@@ -14,17 +14,20 @@ class GFG:
 
             rd, cd = row + x, col + y
             word_len = len(word)
-            
-            for k in range(1, len(word)):
+            k=1
+            while k< word_len:
                 if (rd <0 or rd >= self.R or cd<0 or cd >=self.C):
                     break
                 if (word[k] != matrix[rd][cd]):
                     break
-                print((rd,cd))
+                # print((rd,cd),matrix[rd][cd])
                 rd += x
                 cd += y
+                k+=1
+                # print("updated:",(rd,cd,k))
+                
 
-            if k==word_len:
+            if k == word_len:
                 return True
             
         return False
@@ -33,12 +36,11 @@ class GFG:
         # Rows and columns in given grid
         self.R = len(matrix)
         self.C = len(matrix[0])
-        # Consider every point as starting point
-        # and search given word
+        
         for row in range(self.R):
             for col in range(self.C):
                 
-                print("origin",row,col)
+                # print("origin",row,col)
                 if self.search(matrix, row, col, word):
                     print("pattern found at " +
                            str(row) + ', ' + str(col))
@@ -52,4 +54,3 @@ if __name__=='__main__':
     gfg = GFG()
     gfg.printPosition(matrix, 'ASH')
     print('')
-    # gfg.printPosition(matrix, 'EEE')
