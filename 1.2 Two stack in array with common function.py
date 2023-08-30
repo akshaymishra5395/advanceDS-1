b27e5f9e26f39d52595dfa40c150be017ab2e7dd
akshay <akshaymishra5395@gmail.com>
2023-08-31 00:10:26 +0530

    changes

--- 1.2 Two stack in array with common function.py
+++ 1.2 Two stack in array with common function.py
@@ -2,45 +2,72 @@
 class TwoStacks:
   def __init__(self, n=100):
     self.size = n
-    self.arr = [0] * n
+    self.arr = [None] * n
     self.top1 = -1
     self.top2 = n
 
   def push(self,i,x):
-    if self.top1 !=self.top2 - 1:
+    if self.top1 ==self.top2 - 1:
+      print("Stack "+str(i)+" is Full")
+      return 
+    else:
       if i==1:
         self.top1+=1
         self.arr[self.top1]=x
       else:
         self.top2-=1
-        self.arr[self.top1]=x
+        self.arr[self.top2]=x
+      print(self.arr)
+
   
   def pop(self,i):
     if i==1:
       if self.top1==-1:
+        print("Stack "+str(i)+" is Empty")
         return -1
       else:
         temp = self.arr[self.top1]
+        self.arr[self.top1]=None
         self.top1-=1
+        print(self.arr)
         return temp
     else:
       if self.top2==self.size:
+        print("Stack "+str(i)+" is Empty")
         return -1
       else:
         temp = self.arr[self.top2]
+        self.arr[self.top1]=None
         self.top2+=1
+        print(self.arr)
         return temp
         
 if __name__ == '__main__':
-  sq = TwoStacks()
-  Q = int(input())
-  while Q > 0:
-    query = list(map(int, input().split()))
-    if query[1] == 1:
-      sq.push(query[0],query[2])
-    else:
-      print(sq.pop(query[0]), end=' ')
-    Q -= 1
+  sq = TwoStacks(5)
+  sq.push(1,2)
+  sq.push(1,3)
+  sq.push(1,5)
+  sq.push(2,14)
+  sq.push(2,12)
+  sq.push(2,11)
+  print(sq.pop(1))
+  print(sq.pop(1))
+  print(sq.pop(1))
+  print(sq.pop(1))
+  
+  
+
+  
+  
+  
+  # Q = int(input())
+  # while Q > 0:
+  #   query = list(map(int, input().split()))
+  #   if query[1] == 1:
+  #     sq.push(query[0],query[2])
+  #   else:
+  #     print(sq.pop(query[0]), end=' ')
+  #   Q -= 1
   print()
 
 #number of operations
